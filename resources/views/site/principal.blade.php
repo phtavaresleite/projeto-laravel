0351703a16881f692a2c4983afcb89846e889651
Paulo Henrique <ph.tavaresleite@gmail.com>
2025-04-11 08:53:22 -0300

    Grouped routes in routes/web.php for better organization

--- resources/views/site/principal.blade.php
+++ resources/views/site/principal.blade.php
@@ -1,1 +1,7 @@
 <h3>Principal (view)</h3>
+
+<ul>
+    <li><a href="/">Principal</a></li>
+    <li><a href="/sobrenos">Sobre nos</a></li>
+    <li><a href="/contato">Contato</a></li>
+</ul>
--- routes/web.php
+++ routes/web.php
@@ -25,13 +25,24 @@ options
 
 */
         
-        
+//definindo rotas com controller      
 Route::get('/', [App\Http\Controllers\PrincipalController::class, 'principal']);
-
 Route::get('/sobrenos', [App\Http\Controllers\SobreNosController::class, 'sobreNos']);
-
 Route::get('/contato', [App\Http\Controllers\ContatoController::class, 'contato']);
+Route::get('/login', function(){ return "Login"; });
 
-Route::get('/contato/{nome}/{categoria}/{assunto}/{mensagem?}', function (string $nome, string $categoria, string $assunto, string $mensagem = "Sem mensagem") {
-    return "Estamos aqui " . $nome ."-". $categoria. "-".  $assunto ."-". $mensagem;
+//agrupando rotas
+Route::prefix('/app')->group(function(){
+    Route::get('/clientes', function(){ return "Clientes"; });
+    Route::get('/fornecedores', function(){ return "Fornecedores"; });
+    Route::get('/produtos', function(){ return "Produtos"; });
 });
+
+Route::get('/contato/{nome}/{categoria_id}', function (string $nome, int $categoria = 1 //1 = "informação"
+){
+    return "Estamos aqui $nome - $categoria";
+}) -> where([
+    'categoria' => '[0-9]+',
+])-> where([
+    'nome' => '[A-Za-z]+',
+]);
